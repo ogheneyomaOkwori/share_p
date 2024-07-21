fb125ffabd946aba10c22f89f93fdcb6ffafe572
Ogheneyoma Okwori <Ogheneyoma.Okwori@Interswitchng.com>
2024-07-21 16:54:55 +0100

    update Navbar to fetch profile image add configuration for next/image component hostname

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -56,7 +56,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -87,7 +87,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
